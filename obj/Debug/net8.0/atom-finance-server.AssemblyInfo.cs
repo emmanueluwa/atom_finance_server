d758758e35f7c7b854404baa1ebe10e5e039b4e4
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-07-08 20:31:42 +0100

    adding delete endpoint for stock

--- obj/Debug/net8.0/atom-finance-server.AssemblyInfo.cs
+++ obj/Debug/net8.0/atom-finance-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atom-finance-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b646c609fdae6cd5d46d2d79cd213848720621f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c47aed5d2bc2ac75ae8b7ff2c6c9c5fb622c722")]
 [assembly: System.Reflection.AssemblyProductAttribute("atom-finance-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atom-finance-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
