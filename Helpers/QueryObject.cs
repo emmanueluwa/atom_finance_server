aa21e1dd554c56db40841561987500d4a8b7cc30
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-07-10 00:30:51 +0100

    adding pagination

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -14,5 +14,9 @@ namespace atom_finance_server.Helpers
         public string? SortBy { get; set; } = null;
 
         public bool IsDescending { get; set; } = false;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 20;
     }
 }
